0d92c8204b4b78b7be58f6bde7cd8796bac037f7
Jonathan Tello <jonathantellomarquez@gmail.com>
2026-02-28 23:22:16 -0600

    Add top back button and fullscreen fix

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
-const CACHE_NAME = "rutina-pro-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./assets/sounds/finish.mp3"
-];
+const CACHE_NAME = "rutina-pro-v4";
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./styles.css",
+        "./app.js",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png",
+        "./assets/sounds/finish.mp3"
+      ])
+    )
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
